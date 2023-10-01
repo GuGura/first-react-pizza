bf5363119388dcf607c3c817e865a00d5d88845f
GuGura <wodus331@gmail.com>
2023-10-01 18:08:53 +0900

    text-transform 기억해라

---
+++ src/featrues/user/Username.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Username(props) {
+  return <div className="text-sm font-semibold">Jonas</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import SearchOrder from "../featrues/order/SearchOrder.jsx";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../featrues/order/SearchOrder.jsx';
+import Username from '../featrues/user/Username.jsx';
 
 function Header(props) {
-    return (
-        <header>
-            <Link to='/'>Fast React Pizza Co.</Link>
-            <SearchOrder/>
-            <p>Jonas</p>
-        </header>
-    );
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
 }
 
 export default Header;

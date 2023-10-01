c789b6f96ffa1def34716418edf13b606f926481
GuGura <wodus331@gmail.com>
2023-10-02 01:57:11 +0900

    tailwind end

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
-  const base = `inline-block rounded-full bg-yellow-500 font-semibold uppercase 
+  const base = `inline-block rounded-full bg-yellow-500 text-sm font-semibold uppercase 
   tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring
   focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed`;
 
-  const secondary = `inline-block rounded-full border-2 border-stone-300 bg-transparent font-semibold uppercase 
+  const secondary = `inline-block rounded-full text-sm border-2 border-stone-300 bg-transparent font-semibold uppercase 
   tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring
   focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed hover:text-stone-800 focus:text-stone-800`;
 

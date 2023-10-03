b4cdfdae0ef5675cda41a837a32a487083126196
GuGura <wodus331@gmail.com>
2023-10-04 00:19:09 +0900

    cart redux

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './featrues/user/userSlice.js';
+import cartReducer from './featrues/cart/cartSlice.js';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
